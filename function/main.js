86a9b3d9c26da12c66dd063afee53308773a409d
DowDow0315 <dowdow0315@naver.com>
2026-05-30 00:51:49 +0900

    npc 수정
    : 줄리앙 기본 추가 (대장장이)
    : npc  tab  수정

--- function/main.js
+++ function/main.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     await loadAllNPCData();
     await loadEnemies();
 
-    ["eric", "luke", "sora", "yuri", "matin", "deric", "pale", "nikolai", "valen"].forEach(name => {
+    ["eric", "luke", "sora", "yuri", "matin", "deric", "pale", "nikolai", "valen", "juliang"].forEach(name => {
         registerNPCAsEnemy(name);
     })
 
@@ -2162,7 +2162,7 @@ function saveNpcProgressToLocalStorage(){
 }
 
 async function loadAllNPCData() {
-    const npcList = ["eric", "luke", "sora", "yuri", "matin", "deric", "pale", "nikolai", "valen"];
+    const npcList = ["eric", "luke", "sora", "yuri", "matin", "deric", "pale", "nikolai", "valen", "juliang"];
     const savedNpcData = JSON.parse(localStorage.getItem("npcData") || "null");
 
     for (const name of npcList) {

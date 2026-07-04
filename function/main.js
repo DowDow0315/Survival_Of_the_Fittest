0b7f91fde95fad5fb11bc1ff51e4b2359afd5edc
DowDow0315 <dowdow0315@naver.com>
2026-07-04 18:57:01 +0900

    Update main.js

--- function/main.js
+++ function/main.js
@@ -1979,7 +1979,6 @@ window.travel_whiteFlowerTomb_to_guardPost3 = function(player){
     travelOuterArea(player, "guardPost3", 7);
 };
 
-
 //전투이벤트
 function startBattle(enemyId, player, options = {}){
     const enemyFactory = ENEMIES[enemyId];
